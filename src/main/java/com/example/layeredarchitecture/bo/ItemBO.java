e052e4e858598d3b5730b78f59cfc4386f73dcd9
Tarindhi <tharidithisarani7@gmail.com>
2024-06-22 04:00:20 +0530

    18th class works

--- src/main/java/com/example/layeredarchitecture/bo/ItemBO.java
+++ src/main/java/com/example/layeredarchitecture/bo/ItemBO.java
@@ -1,4 +1,25 @@
 package com.example.layeredarchitecture.bo;
 
+import com.example.layeredarchitecture.dao.SqlUtil;
+import com.example.layeredarchitecture.model.ItemDTO;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface ItemBO {
+    public ArrayList<ItemDTO> getAll() throws SQLException, ClassNotFoundException;
+
+    public boolean delete(String code) throws SQLException, ClassNotFoundException;
+
+    public boolean add(ItemDTO dto) throws SQLException, ClassNotFoundException;
+
+    public boolean update(ItemDTO dto) throws SQLException, ClassNotFoundException;
+
+    public boolean exist(String code) throws SQLException, ClassNotFoundException;
+
+    public String generateNewID() throws SQLException, ClassNotFoundException;
+
+    public ItemDTO search(String code) throws SQLException, ClassNotFoundException;
+
 }
